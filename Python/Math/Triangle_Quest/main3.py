56044003882473fbececb479262f833c9b0a88af
Julien Cretel <j.cretel@umail.ucc.ie>
2015-12-14 08:32:11 +0000

    Add Python 2 & 3 solutions to 'Triangle Quest'

---
+++ Python/Math/Triangle_Quest/main3.py
@@ -0,0 +1,6 @@
+# Link: https://www.hackerrank.com/challenges/python-quest-1
+
+# Note: Submit only the two lines of code; remove the last EOL.
+
+for i in range(1, int(input())):
+    print(i * sum(map(lambda k: 10 ** k, range(0, i))))
