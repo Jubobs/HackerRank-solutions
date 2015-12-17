aa20c551ada762dbd70cc6943422b65909faaf66
Julien Cretel <j.cretel@umail.ucc.ie>
2015-12-17 14:02:01 +0100

    Add Python-2 solution to 'Integers Come In All Sizes'

---
+++ Python/Introduction/Integers_Come_In_All_Sizes/main2.py
@@ -0,0 +1,8 @@
+# Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes
+
+if __name__ == "__main__":
+    a = int(raw_input())
+    b = int(raw_input())
+    c = int(raw_input())
+    d = int(raw_input())
+    print a ** b + c ** d
