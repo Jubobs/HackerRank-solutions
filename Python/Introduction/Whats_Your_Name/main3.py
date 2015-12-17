5f8a51737f5f7825466728de7d57574c537db5a8
Julien Cretel <j.cretel@umail.ucc.ie>
2015-12-17 16:35:59 +0100

    Add Python 2 & 3 solutions to 'What\'s Your Name?'

---
+++ Python/Introduction/Whats_Your_Name/main3.py
@@ -0,0 +1,6 @@
+# Link: https://www.hackerrank.com/challenges/whats-your-name
+
+if __name__ == "__main__":
+    first = input()
+    last = input()
+    print("Hello " + first + " " + last + "! You just delved into python.")
